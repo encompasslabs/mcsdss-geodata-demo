2f0d8930e47d40d21aed0ab7cfe1ef5e4024f2ef
Encompass Labs <encompasslabs@gmail.com>
2015-03-13 14:07:23 -0500

    Update mcsdss-geodata.js

--- mcsdss-geodata.js
+++ mcsdss-geodata.js
@@ -1,2 +1,14 @@
 // JS code for the MCSDSS GeoData Demo.
 console.log('mcsdss-geodata demo javascript.');
+
+window.addEvent('domready', function() {
+	new Request.HTML({
+		url: '/gh/get/response.html/encompasslabs/mcsdss-geodata-demo/tree/master/',
+		data: {'delay': 1},
+		method: 'post',
+		update: 'demo',
+		onSuccess: function(response) {
+			$('map').highlight();
+		}
+	}).send();
+})
